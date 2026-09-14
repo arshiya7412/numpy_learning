2cc3eb66cc3a0fb01ad69cc2e101d5c999968f25
Arshiya Sana <arshiyasana2006@gmail.com>
2026-09-14 23:31:35 +0530

    Update matrix_addition.py

--- matrix_addition.py
+++ matrix_addition.py
@@ -4,3 +4,13 @@ v = np.array([[5, 6],
               [7, 8]])
 print(d + 10)
 print(d )
+
+## another method
+
+import numpy as np
+
+v1 = np.array([1, 2, 3])
+v2 = np.array([4, 5, 6])
+
+print(v1 + v2)
+print(np.dot(v1, v2))
